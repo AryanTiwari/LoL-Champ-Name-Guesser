1d6362dff3603e3c42ca0b46f180a4d2ecbc6acd
AryanTiwari <powerdrgn@gmail.com>
2018-12-16 11:54:24 -0500

    Initial Commit

--- src/App.js
+++ src/App.js
@@ -1,28 +1,37 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import { Input } from 'antd';
 import './App.css';
 
+const championData = require('./data/championData.json').data;
+const championNames = Object.keys(championData);
+
 class App extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      championName: this._generateRandomChamp() 
+    }
+  }
+
+  _generateRandomChamp = () => {
+    const randomNumber = Math.round(Math.random() * championNames.length);
+    const randomChampion = championNames[randomNumber];
+    return randomChampion;
+  }
+
+  _checkName = (e) => {
+    console.log(e);
+  }
+
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+        <p> League of Legends Champion Guesser </p>
+        <p> {this.state.championData} </p>
+        <Input style={{width:200}} placeholder="Champion Name" onPressEnter={this._checkName}/>
       </div>
     );
   }
-}
+} 
 
 export default App;
